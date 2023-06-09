f3f9fa81409bd3c9449dbb72b32c220104cc6a3d
shikhar2624 <shikhargupta2624@gmail.com>
2023-06-09 18:15:56 +0530

    some more edit

--- gstreamer_send.py
+++ gstreamer_send.py
@@ -6,7 +6,7 @@ from gi.repository import Gst
 Gst.init(None)
 
 # Create the pipeline
-pipeline = Gst.parse_launch("v4l2src device=/dev/video0 ! image/jpeg,width=1280,height=720,framerate=30/1 ! rtpjpegpay ! udpsink host=$myip port=$myport")
+pipeline = Gst.parse_launch("v4l2src device=/dev/video0 ! image/jpeg,width=1280,height=720,framerate=30/1 ! rtpjpegpay ! udpsink host=192.168.0.58 port=8000")
 
 # Start the pipeline
 pipeline.set_state(Gst.State.PLAYING)
