bf80dbc0f3ddbe8a2ab48153d7e541d738b85545
shikhar2624 <shikhargupta2624@gmail.com>
2023-06-13 11:53:15 +0530

    updating

--- flask_main.py
+++ flask_main.py
@@ -20,9 +20,9 @@ def generate_frames():
     #     if not success:
     #         break
     #     else:
-    print(frame," frames")
-    ret, buffer = cv2.imencode('.jpg', frame)
-    frame = buffer.tobytes()
+    # print(frame," frames")
+    # ret, buffer = cv2.imencode('.jpg', frame)
+    # frame = buffer.tobytes()
     yield (b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
